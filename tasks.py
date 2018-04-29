a8fc5bd796eba8f0af8bfad396af0de4461c66af
Luc Saffre <luc.saffre@gmail.com>
2018-04-29 18:50:05 +0300

    http://luc.lino-framework.org/blog/2018/0429.html

--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
-from atelier.invlib.ns import ns
-ns.setup_from_tasks(globals(), "commondata.ee")
+from atelier.invlib import setup_from_tasks
+ns = setup_from_tasks(globals(), "commondata.ee")
 ns.configure({
     'revision_control_system': 'git',
-    'docs': [],
 })

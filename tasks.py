6318e940d40f15eb570a8325391172d1fc9570ba
Luc Saffre <luc.saffre@gmail.com>
2021-04-09 13:20:21 +0300

    update tasks.py syntax

--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
 from atelier.invlib import setup_from_tasks
-ns = setup_from_tasks(globals(), "commondata.ee")
-ns.configure({
-    'revision_control_system' : 'git',
-    # 'doc_trees' : []
-})
+ns = setup_from_tasks(globals(), "commondata.ee",
+    revision_control_system='git'
+    )
